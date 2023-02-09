f5804b85ccf273e87a7fa1f0dcdf1492a8dbc80a
Tomas Blažauskas <blazauskas.tomas@gmail.com>
2023-02-09 21:28:58 +0200

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,6 +10,17 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+         [Required]
+        public string Gender { get; set; }
+         [Required]
+        public DateOnly? DateOfBirth { get; set; } //optinal to make required work(DateOnly cannot be null, so for required tag we need to make it optional)
+         [Required]
+        public string City { get; set; }
+         [Required]
+        public string Country { get; set; }
+
         [StringLength(8, MinimumLength = 4)]
         [Required]
         public string Password { get; set; }
